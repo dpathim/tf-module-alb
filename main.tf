65c8b6c01c7a60a73fb54d278c024db1a769279e
Deepthi <dpdevops6@gmail.com>
2024-02-28 21:44:18 -0500

    Terraform

--- main.tf
+++ main.tf
@@ -4,30 +4,8 @@ resource "aws_lb" "main" {
   load_balancer_type = var.lb_type
   security_groups    = [aws_security_group.main.id]
   subnets            = var.subnets
-  tags = merge(local.tags, {Name = "${var.env}-alb"})
+  tags = merge(local.tags, { Name = "${var.env}-alb" })
 }
-
-#resource "aws_security_group" "main" {
-#  name        = local.sg_name
-#  description = local.sg_name
-#  vpc_id      = var.vpc_id
-#  tags        = merge(local.tags, { Name = local.sg_name })
-  #
-  #  ingress {
-  #    description = "APP"
-  #    from_port         = var.sg_port
-  #    to_port           = var.sg_port
-  #    protocol       = "tcp"
-  #    cidr_blocks        = var.sg_ingress_cidr
-  #  }
-  # egress {
-  #   from_port         = 0
-  #   to_port           = 0
-  #   protocol       = "-1"
-  #   cidr_blocks        = ["0.0.0.0/0"]
-  #  ipv6_cidr_blocks        = ["::/0"]
-  # }
-  #}
 
 resource "aws_security_group" "main" {
   name        = local.sg_name
@@ -35,32 +13,56 @@ resource "aws_security_group" "main" {
   vpc_id      = var.vpc_id
   tags        = merge(local.tags, { Name = local.sg_name })
 }
+resource "aws_security_group" "main" {
+  name        = local.sg_name
+  description = local.sg_name
+  vpc_id      = var.vpc_id
+  tags        = merge(local.tags, { Name = local.sg_name })
 
-  resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-    description       = "APP"
-    security_group_id = aws_security_group.main.id
-    cidr_ipv4         = var.sg_ingress_cidr
-    from_port         = var.sg_port
-    ip_protocol       = "tcp"
-    to_port           = var.sg_port
+  ingress {
+    description = "APP"
+    from_port   = var.sg_port
+    to_port     = var.sg_port
+    protocol    = "tcp"
+    cidr_blocks = var.sg_ingress_cidr
   }
-  resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-    security_group_id = aws_security_group.main.id
-    cidr_ipv4         = var.sg_ingress_cidr
-    from_port         = var.sg_port
-    ip_protocol       = "tcp"
-    to_port           = var.sg_port
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
+}
 
 
-  resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-    security_group_id = aws_security_group.main.id
-    cidr_ipv4         = "0.0.0.0/0"
-    ip_protocol       = "-1" # semantically equivalent to all ports
-  }
 
-  resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-    security_group_id = aws_security_group.main.id
-    cidr_ipv6         = "::/0"
-    ip_protocol       = "-1" # semantically equivalent to all ports
-  }
+
+#  resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+#    description       = "APP"
+#    security_group_id = aws_security_group.main.id
+#    cidr_ipv4         = var.sg_ingress_cidr
+#    from_port         = var.sg_port
+#    ip_protocol       = "tcp"
+#    to_port           = var.sg_port
+#  }
+#  resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+#    security_group_id = aws_security_group.main.id
+#    cidr_ipv4         = var.sg_ingress_cidr
+#    from_port         = var.sg_port
+#    ip_protocol       = "tcp"
+#    to_port           = var.sg_port
+#  }
+#
+#
+#  resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#    security_group_id = aws_security_group.main.id
+#    cidr_ipv4         = "0.0.0.0/0"
+#    ip_protocol       = "-1" # semantically equivalent to all ports
+#  }
+#
+#  resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+#    security_group_id = aws_security_group.main.id
+#    cidr_ipv6         = "::/0"
+#    ip_protocol       = "-1" # semantically equivalent to all ports
+#  }
